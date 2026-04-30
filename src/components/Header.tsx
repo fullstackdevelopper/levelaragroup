9a77843aa7ee597fd0129f791e7e8d8ac5a5843a
fullstackdevelopper <berlenskystanley8686@gmail.com>
2026-04-30 14:48:24 -0400

    Update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,10 @@
 import Image from "next/image"
 import { useEffect, useRef } from "react";
 import Typed from 'typed.js'
-
+import Link from "next/link";
 export default function Header() {
 
- const el = useRef<HTMLSpanElement | null>(null);
+  const el = useRef<HTMLSpanElement | null>(null);
   const typedInstance = useRef<Typed | null>(null);
 
   useEffect(() => {
@@ -30,18 +30,24 @@ export default function Header() {
     };
   }, []);
 
-    return (
-        <>
-            <section id="hero" className="hero section dark-background">
-                <Image src="/img/18516.jpg" alt="" data-aos="fade-in" className="" width={1500} height={1000} />
+  return (
+    <>
+      <section id="hero" className="hero section dark-background">
+        <Image src="/img/18516.jpg" alt="" data-aos="fade-in" className="" width={1500} height={1000} />
 
-                <div className="container" data-aos="fade-up" data-aos-delay="100">
-                    <h2>LevelARA Group</h2>
-                    <p>
-                        <span className="typed" ref={el}>LevelARA Group</span>
-                    </p>
-                </div>
-            </section >
-        </>
-    )
+        <div className="container" data-aos="fade-up" data-aos-delay="100">
+          <h2>LevelARA Group</h2>
+          <p>
+            <span className="typed" ref={el}>LevelARA Group</span>
+          </p>
+          <Link href="/contact" >
+            <button className="btn btn-info btn-contact btn-lg text-white shadow mt-3 fw-bold">
+              <i className="bi bi-telephone me-2"></i>
+              Contactez nous
+            </button>
+          </Link>
+        </div>
+      </section >
+    </>
+  )
 }
